9862e1e9c9038058545cce8e8537ad7adff26a6b
Julien Moes <julien.moes@gmail.com>
2019-06-25 13:46:38 +0200

    Rework of the project base.

---
+++ Discord Hack Week Moderation Bot/Moderation Bot [LINUX]/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Moderation_Bot__LINUX_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moderation_Bot__WINDOWS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
