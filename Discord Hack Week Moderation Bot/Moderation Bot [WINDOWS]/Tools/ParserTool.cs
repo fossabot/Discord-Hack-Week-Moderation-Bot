a277423bfd02326adf13c79a3c8d2d29dad8e077
Julien Moes <julien.moes@gmail.com>
2019-06-26 01:43:01 +0200

    Add Homemade String Parser

---
+++ Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Tools/ParserTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Moderation_Bot__WINDOWS_.Tools
+{
+    public class ParserTool
+    {
+        public string Parse(string text, string start, string end)
+        {
+            int i = 0;
+            int space = 0;
+
+            for (; text.Substring(i, start.Length) != start; i++);
+            for (int count = 0; count != start.Length + 1; count++);
+            for (i += start.Length; text.Substring(i, end.Length) != end; i++)
+                space++;
+            string Result = text.Substring(i - space, space);
+
+            return (Result);
+        }
+    }
+}
