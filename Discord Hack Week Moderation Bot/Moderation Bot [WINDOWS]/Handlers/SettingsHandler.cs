639c4c7d3e7ab0a33491b990f416fbb92a57f984
Julien Moes <julien.moes@gmail.com>
2019-06-25 19:30:27 +0200

    Edit Settings Handler

--- Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Handlers/SettingsHandler.cs
+++ Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Handlers/SettingsHandler.cs
@@ -5,5 +5,6 @@ namespace Moderation_Bot__WINDOWS_.Handlers
     public class SettingsHandler
     {
         public string BotToken { get; } = ConfigurationManager.AppSettings["BotToken"];
+        public string BotCommandPrefix { get; } = ConfigurationManager.AppSettings["BotCommandPrefix"];
     }
 }
