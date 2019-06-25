472a1d441449c89be453cdcede0159f46cf442a7
Julien Moes <julien.moes@gmail.com>
2019-06-25 19:30:46 +0200

    Edit

--- Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Program.cs
+++ Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Program.cs
@@ -26,9 +26,11 @@ namespace Moderation_Bot__WINDOWS_
             await _Client.LoginAsync(TokenType.Bot, _Settings.BotToken);
             await _Client.StartAsync();
 
-            await _Service.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null); ;
+            await _Service.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
 
             _Client.Log += _Client_Log;
+            _Client.LatencyUpdated += _Client_LatencyUpdated;
+            _Client.MessageReceived += _Client_MessageReceived;
 
             await Task.Delay(-1);
         }
@@ -36,6 +38,16 @@ namespace Moderation_Bot__WINDOWS_
         private async Task _Client_Log(LogMessage arg)
         {
             await new LogsHandler().Logs(arg);
+        }
+
+        private async Task _Client_LatencyUpdated(int arg1, int arg2)
+        {
+            await new BotStateHandler().BotState(_Client, arg2, _Settings.BotCommandPrefix);
+        }
+
+        private async Task _Client_MessageReceived(SocketMessage arg)
+        {
+            await new MessagesHandler().Messages(arg, _Client, _Service, _Settings.BotCommandPrefix);
         }
     }
 }
