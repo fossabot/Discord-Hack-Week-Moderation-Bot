8ef15af72e7b9fd22cd8c68d97c937343503c445
Julien Moes <julien.moes@gmail.com>
2019-06-26 01:41:32 +0200

    Add Mute and Unmute Commands

---
+++ Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Commands/Administrators/MuteCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+using Moderation_Bot__WINDOWS_.Handlers;
+
+using Discord;
+using Discord.Commands;
+
+namespace Moderation_Bot__WINDOWS_.Commands.Administrators
+{
+    public class MuteCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("mute")]
+        [RequireBotPermission(GuildPermission.MuteMembers)]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        public async Task Mute(IGuildUser UserName)
+        {
+            await new MuteStateHandler().Mute(UserName, Context);
+        }
+    }
+}
---
+++ Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Commands/Administrators/UnmuteCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+using Moderation_Bot__WINDOWS_.Handlers;
+
+using Discord;
+using Discord.Commands;
+
+namespace Moderation_Bot__WINDOWS_.Commands.Administrators
+{
+    public class UnmuteCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("unmute")]
+        [RequireBotPermission(GuildPermission.MuteMembers)]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        public async Task Unmute(IGuildUser UserName)
+        {
+            await new MuteStateHandler().Unmute(UserName, Context);
+        }
+    }
+}
