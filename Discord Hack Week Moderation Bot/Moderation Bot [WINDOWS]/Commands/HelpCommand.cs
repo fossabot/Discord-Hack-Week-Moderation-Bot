5732b2bf21b4b55f3b2ea3ac9eab8423b7f41c3f
Julien Moes <julien.moes@gmail.com>
2019-06-27 07:49:58 +0200

    Edit mute

--- Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Commands/HelpCommand.cs
+++ Discord Hack Week Moderation Bot/Moderation Bot [WINDOWS]/Commands/HelpCommand.cs
@@ -27,6 +27,8 @@ namespace Moderation_Bot__WINDOWS_.Commands
                 embedBuilder.WithTitle("Here is the commands list !")
                 .AddField(".ban [UserName] [DeleteMsgDay's] [Reason]", "Ban the user by deleting his messages on the number of days wanted while giving him a reason for the ban !")
                 .AddField(".kick [UserName] [Reason]", "kick the user while giving him a reason for the kick !")
+                .AddField(".mute [UserName]", "mute the user selected !")
+                .AddField(".unmute [UserName]", "unmute the user selected !")
                 .WithColor(Color.DarkRed);
             }
             else
